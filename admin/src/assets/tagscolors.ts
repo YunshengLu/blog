f050fa63ffa87381f3f7c9866119b32dcde6f3e8
YunshengLu <934201509@qq.com>
2022-09-01 22:00:49 +0800

    静态资源

---
+++ admin/src/assets/tagscolors.ts
@@ -0,0 +1,42 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: LuyunSheng
+ * @Date: 2022-08-28 23:52:37
+ * @LastEditTime: 2022-08-28 23:58:54
+ */
+const COLORS = [
+    'red',
+    'orangered',
+    'orange',
+    'gold',
+    'lime',
+    'green',
+    'cyan',
+    'blue',
+    'arcoblue',
+    'purple',
+    'pinkpurple',
+    'magenta',
+    'gray',
+];
+const COLORS_CUSTOM = [
+    '#f53f3f',
+    '#7816ff',
+    '#00b42a',
+    '#165dff',
+    '#ff7d00',
+    '#eb0aa4',
+    '#7bc616',
+    '#86909c',
+    '#b71de8',
+    '#0fc6c2',
+    '#ffb400',
+    '#168cff',
+    '#ff5722',
+];
+
+export {
+    COLORS,
+    COLORS_CUSTOM,
+}
