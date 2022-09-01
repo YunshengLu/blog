9ccd1b7a8338f834695e6eec5e94aee08ac18af1
YunshengLu <934201509@qq.com>
2022-09-01 22:08:31 +0800

    标签管理

---
+++ admin/src/pages/tags/store/actionTypes.ts
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: LuyunSheng
+ * @Date: 2022-08-24 23:42:26
+ * @LastEditTime: 2022-08-26 02:04:06
+ */
+// 更新列表
+export const UPDATE_LIST = 'tags_update_list';
+// loading
+export const UPDATE_LOADING = 'tags_update_loading';
+// 分页
+export const UPDATE_PAGINATION = 'tags_update_pagination';
+// 
+export const UPDATE_FORM_PARAMS = 'tags_update_formParams';
+// 切换弹窗显示状态
+export const TOGGLE_VISIBLE = 'tags_toggle_visible';
+// loading
+export const TOGGLE_CONFIRM_LOADING = 'tags_toggle_confirm_loading';
